3d2620b20a6460c506626063a72446780c664354
Adrian HEOUAIRI <adrian.heouairi@etu.u-paris.fr>
2023-04-09 12:33:42 +0200

    Improve release script

--- release.sh
+++ release.sh
@@ -4,49 +4,59 @@
 
 set -e
 
-if ! [ "$2" ]; then
-    echo "Usage: ${0##*/} <gaufre token> <release description> [additional files for the zip]..."
+error() {
+    echo "Error: $*"
+    exit 1
+}
+
+if ! [ "$3" ]; then
+    echo "Usage: ${0##*/} <gaufre token> <version> <release description> [additional files for the zip]..."
+    echo "Additional file example: build/jacoco/test.exec"
     echo "You can create a token in your GitLab settings"
     echo "To make a release: manually merge develop into master, then run this script."
     exit 1
 fi
 
 token=$1
-description=$2
-shift 2
+version=${2#v}
+description=$3
+shift 3
 
-[ "$(which curl)" ] || { echo 'You have to install curl'; exit 1; }
+[ "$(which curl)" ] || error 'You have to install curl'
 
 master=$(git rev-parse --show-toplevel)
 cd -- "$master"
 
-[ -d .git ] || { echo "Don't run this script in a worktree"; exit 1; }
+[ -d .git ] || error "Don't run this script in a worktree"
 
-[ "$(git branch --show-current)" = master ] || { echo "Run the script on branch master"; exit 1; }
+git fetch
 
-git pull --rebase
+[ "$(git branch --show-current)" = master ] || error "Run the script on branch master"
+
+git pull
 ./gradlew build
 
-version=$(sed -En "s/^version +['\"](.+)['\"]$/\1/p" build.gradle)
-[ "$version" ]
+#version=$(sed -En "s/^version +['\"](.+)['\"]$/\1/p" build.gradle)
+#[ "$version" ]
 
 artifacts="$master"_worktree
 [ -e "$artifacts" ] || git worktree add "$artifacts" artifacts
 
 cd "$artifacts"
-git pull --rebase
+git pull
 [ -e artifacts ] || mkdir artifacts
 cd artifacts
+
 zip=MoriaMap-v"$version".zip
-[ -e "$zip" ] && { echo "Zip already exists"; exit 1; }
+if ! [ -e "$zip" ]; then
+    files=()
+    for i; do files+=("$master/$i"); done
 
-files=()
-for i; do files+=("$master/$i"); done
-
-zip -j "$zip" "$master/build/libs/MoriaMap-$version.jar" "$master/Instructions.txt" "${files[@]}"
-git add "$zip"
-git commit -m "Add release zip for v$version"
-git push
+    zip -j "$zip" "$master/build/libs/MoriaMap.jar" "$master/Instructions.txt" "$master/MoriaMap.sh" "${files[@]}"
+    git add "$zip"
+    git commit -m "Add release zip for v$version"
+    git push
+fi
 
 data='{ "name": "MoriaMap v'$version'", "tag_name": "v'$version'", "description": "'$description'", "ref": "master", "assets": { "links": [{ "name": "Zip archive with jar", "url": "https://gaufre.informatique.univ-paris-diderot.fr/tazouev/moriamap/raw/artifacts/artifacts/'$zip'?inline=false" }] } }'
 
